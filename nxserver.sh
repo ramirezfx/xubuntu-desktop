809a92d2038f945c1492f87852112758a782273a
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-05 16:07:50 +0100

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -3,8 +3,8 @@ groupadd -r $USER -g 433 \
 && useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER \
 && adduser $USER sudo \
 && mkdir /home/$USER \
+&& echo "setxkbmap de" > /home/$USER/.bashrc \
 && chown -R $USER:$USER /home/$USER \
 && echo $USER':'$PASSWORD | chpasswd
-&& echo "setxkbmap de" > /home/$USER/.bashrc
 service ssh start && /etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
