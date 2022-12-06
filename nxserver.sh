58badbb088a0985b2d19cefcfb053e3fc4a89d30
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-06 06:48:15 +0100

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -3,7 +3,7 @@ groupadd -r $USER -g 433 \
 && useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER \
 && adduser $USER sudo \
 && mkdir /home/$USER \
-&& echo "setxkbmap de" > /home/$USER/.bashrc \
+&& echo "setxkbmap "$KBD_LANG > /home/$USER/.bashrc \
 && chown -R $USER:$USER /home/$USER \
 && echo $USER':'$PASSWORD | chpasswd
 service ssh start && /etc/NX/nxserver --startup
