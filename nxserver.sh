fb19d62b815a8ac90744e1b584a0da4a8a82551f
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-06 08:00:41 +0100

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -6,5 +6,9 @@ groupadd -r $USER -g 433 \
 && echo "setxkbmap "$KBD_LANG > /home/$USER/.bashrc \
 && chown -R $USER:$USER /home/$USER \
 && echo $USER':'$PASSWORD | chpasswd
+echo $LANG UTF-8 > /etc/locale.gen && \
+    env DEBIAN_FRONTEND=noninteractive apt-get install -y \
+     locales && \
+     locale-gen $LANG || update-locale --reset LANG=$LANG
 service ssh start && /etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
