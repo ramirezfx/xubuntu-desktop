effb207353005f31f526c11b08ac4a62f769ebac
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-18 23:27:34 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,9 +1,11 @@
 # Download and install latest Google Chrome-Browser
+# -------------------------------------------------
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb && \
 apt-get install -y /tmp/google-chrome-stable_current_amd64.deb
 
 # Install latest Firefox-Browser
+# ------------------------------
 
 snap remove firefox
 apt-get remove -y firefox
@@ -13,3 +15,11 @@ echo "Pin: release o=LP-PPA-mozillateam" >> /etc/apt/preferences.d/mozilla-firef
 echo "Pin-Priority: 1001" >> /etc/apt/preferences.d/mozilla-firefox
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 apt install -y firefox
+
+# Install Seafile-Cloud-Sync-Client
+# ---------------------------------
+
+wget https://linux-clients.seafile.com/seafile.asc -O /usr/share/keyrings/seafile-keyring.asc
+bash -c "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/seafile-keyring.asc] https://linux-clients.seafile.com/seafile-deb/jammy/ stable main' > /etc/apt/sources.list.d/seafile.list"
+apt update
+apt install -y seafile-gui
