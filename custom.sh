ea1b3ad3bdb6211103601a5c22fa43d47477c1db
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-18 22:39:15 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,1 +1,4 @@
-a
+# Download latest Google Chrome-Browser
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb && \
+apt-get install -y /tmp/google-chrome-stable_current_amd64.deb
