8646844d016300dfac4cc6f510a0398bf1f0da8c
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-18 23:13:48 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -6,3 +6,10 @@ apt-get install -y /tmp/google-chrome-stable_current_amd64.deb
 # Install latest Firefox-Browser
 
 snap remove firefox
+apt-get remove -y firefox
+add-apt-repository -y ppa:mozillateam/ppa
+echo "Package: *" > /etc/apt/preferences.d/mozilla-firefox
+echo "Pin: release o=LP-PPA-mozillateam" >> /etc/apt/preferences.d/mozilla-firefox
+echo "Pin-Priority: 1001" >> /etc/apt/preferences.d/mozilla-firefox
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+apt install -y firefox
