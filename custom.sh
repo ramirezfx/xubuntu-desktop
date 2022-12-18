bdc7a7c0101c58326a3638fc7fa8f6ddd842f9e0
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-18 22:55:42 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,4 +1,8 @@
-# Download latest Google Chrome-Browser
+# Download and install latest Google Chrome-Browser
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb && \
 apt-get install -y /tmp/google-chrome-stable_current_amd64.deb
+
+# Install latest Firefox-Browser
+
+snap remove firefox
