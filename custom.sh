0c8fff8f93f0828f8169c3e9216534808e0553e1
Lorenzo Mueller <lorenzomueller1209@gmail.com>
2023-01-14 21:22:04 +0100

    Updated

--- custom.sh
+++ custom.sh
@@ -1,10 +1,4 @@
-# Download and install latest Google Chrome-Browser
-# -------------------------------------------------
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb && \
-apt-get install -y /tmp/google-chrome-stable_current_amd64.deb
-
-# Install latest Firefox-Browser
+# Renove Snap-Version of Firefox and Install latest DEB-Version
 # ------------------------------
 
 snap remove firefox
@@ -15,18 +9,37 @@ echo "Pin: release o=LP-PPA-mozillateam" >> /etc/apt/preferences.d/mozilla-firef
 echo "Pin-Priority: 1001" >> /etc/apt/preferences.d/mozilla-firefox
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 apt install -y firefox
+mv /usr/share/applications/firefox.desktop /usr/share/applications/static-firefox.desktop
+
+# Download and install latest Google Chrome-Browser
+# -------------------------------------------------
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb && \
+apt-get install -y /tmp/google-chrome-stable_current_amd64.deb
+rm /tmp/google-chrome-stable_current_amd64.deb
 
 # Install Seafile-Cloud-Sync-Client
 # ---------------------------------
 
 wget https://linux-clients.seafile.com/seafile.asc -O /usr/share/keyrings/seafile-keyring.asc
-bash -c "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/seafile-keyring.asc] https://linux-clients.seafile.com/seafile-deb/jammy/ stable main' > /etc/apt/sources.list.d/seafile.list"
+OSRELEASE=$(cat /etc/os-release | grep UBUNTU_CODENAME | cut -d "=" -f2)
+if [ $OSRELEASE == 'jammy'  ]
+then
+ echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/seafile-keyring.asc] https://linux-clients.seafile.com/seafile-deb/jammy/ stable main' > /etc/apt/sources.list.d/seafile.list
+fi
 apt update
 apt install -y seafile-gui
 
+# Install Mobirise Website-Builder
+# --------------------------------
+
+DLLINK=$(wget --save-headers --output-document - https://mobirise.com/history.html | grep .deb | grep -Eo 'https://[^ >]+'|head -1 | cut -d'"' -f 1)
+wget -O /tmp/mobirise-latest.deb $DLLINK
+apt-get install -y /tmp/mobirise-latest.deb
+rm /tmp/mobirise-latest.deb
+
 # Cleanup
 # -------
-
-rm -Rf /var/cache/apt/archives/
-apt-get remove -y snapd
+apt remove snapd
+rm -Rf /var/cache/apt/archives
 rm -Rf /var/lib/snapd
--- nxserver.sh
+++ nxserver.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
+
+# Create User and Group:
 groupadd -r $USER -g 433 \
 && useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER \
 && adduser $USER sudo \
 && mkdir /home/$USER \
-&& echo "setxkbmap "$KBD_LANG > /home/$USER/.bashrc \
 && chown -R $USER:$USER /home/$USER \
 && echo $USER':'$PASSWORD | chpasswd
-echo $LANG UTF8 > /etc/locale.gen && \
-     update-locale LANG=$LANG LANGUAGE
+userdel -r mate
+
+# Set Timezone
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-service ssh start && /etc/NX/nxserver --startup
+
+# Set System-Language:
+echo $LANG UTF8 > /etc/locale.gen && update-locale LANG=$LANG LANGUAGE
+
+# Start nxserver-software
+/etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
